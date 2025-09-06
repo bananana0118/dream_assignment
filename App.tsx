89ca24de56d0a23bd991bbf544a237d8b88a5b87
bananana0118 <bananana0118@gmail.com>
2025-09-06 15:10:57 +0900

    setting: initialize expo project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Counter } from "./src/components/Counter";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Counter />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
+function atom(arg0: number) {
+    throw new Error("Function not implemented.");
+}
+
+function useAtom(countAtom: any): [any, any] {
+    throw new Error("Function not implemented.");
+}
