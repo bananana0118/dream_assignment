0e3bec30c54d33871ad5a3a373847c6de19f258f
bananana0118 <bananana0118@gmail.com>
2025-09-13 22:47:33 +0900

    [feat]: 타입에러 수정

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-export { default } from "expo-router/entry";
-
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// src/types/expo-router.d.ts
+declare module "expo-router/entry" {
+  const Entry: any; // 필요하면 any 대신 타입을 확장 가능
+  export default Entry;
+}
